28c860f8406a9aa2f88630275c85761a2964a1d3
Vitalii <100052082+lvtt19@users.noreply.github.com>
2022-04-15 20:02:28 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-var rellax=new Rellax(".rellax");
+var rellax = new Rellax(".rellax");
+
+document.body.onload = function () {
+   setTimeout(function () {
+      var loader = document.getElementById('page-loader');
+      if (!loader.classList.contains('done')) {
+         loader.classList.add('done');
+      }
+   }, 2000);
+}
